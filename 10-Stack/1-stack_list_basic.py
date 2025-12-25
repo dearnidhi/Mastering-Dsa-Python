12a7622c7d87736f3208e6bc8f2c9836bdf4a27f
Nidhi Chouhan <nidhiyachouhan12@gmail.com>
2025-12-25 21:19:13 +0530

    Updated code

--- 10-Stack/1-stack_list_basic.py
+++ 10-Stack/1-stack_list_basic.py
@@ -2,45 +2,41 @@
 class Stack:
     def __init__(self):
         self.list = []
-
     def __str__(self):
-        value = [str(x) for x in reversed(self.list)]
+        value = [str(x) for x in reversed(self.list)]  
         return '\n'.join(value)
-    
+
     def isEmpty(self):
-        return self.list == []
-    
-        
-    
+        return self.list == []  
     def push(self, value):
         self.list.append(value)
-        return "inseted"
-
-       
+        return "inserted"
     def pop(self):
         if self.isEmpty():
-           return "stack is Empty"
-
+            return "it is Empty"
         return self.list.pop()
-
     def peek(self):
         if self.isEmpty():
-            return "stack is Empty"  
+            return "Stack is Empty"
         return self.list[-1]
-    
     def delete(self):
-        self.list == []  
+        self.list == []
 
     def size(self):
-        return len(self.list) 
-
+        return len(self.list)    
 customStack = Stack()  
-customStack.push(1)       
-customStack.push(2) 
-customStack.push(3) 
-customStack.push(4) 
-customStack.push(5) 
-print(customStack.peek())
-print(customStack)     
-print(customStack.size())
-print(customStack.delete())
+customStack.push(1)
+customStack.push(2)
+customStack.push(3)
+customStack.push(4)
+print(customStack)
+
+print("peek",customStack.peek())
+print("size",customStack.size())
+print("after delete",customStack.delete())
+
+
+
+
+
+
--- 10-Stack/2-stack_list_maxsize.py
+++ 10-Stack/2-stack_list_maxsize.py
@@ -1,48 +1,40 @@
 # (Stack with max size)
 # isEmpty
-
 class stack:
-    def __init__(self , maxsize):
+    def __init__(self, maxsize):
         self.maxsize = maxsize
         self.list = []
-
     def __str__(self):
-        values = [str(x) for x in reversed(self.list)]
-        return '\n'.join(values)
+        values = [str(x) for x in reversed(self.list)] 
+        return '\n'.join(values)  
     
     def isEmpty(self):
         return self.list == []
-    
     def isFull(self):
         return len(self.list) == self.maxsize
-    
     def push(self, values):
         if self.isFull():
-            return "Stack is Full"
+            return "Stack is full"
         self.list.append(values)
-        return "Inserted"
-    
+        return "inserted"
     def pop(self):
         if self.isEmpty():
-            return "Stack is Empty"
+            return "Stack is isEmpty"
         return self.list.pop()
-    
     def peek(self):
         if self.isEmpty():
-            return "stack is Empty"
+            return "Stack is isEmpty"
         return self.list[-1]
-    
     def delete(self):
         self.list = []
-
-
-customstack = stack(4)
-
+customstack=stack(4)    
 print(customstack.isEmpty)
 customstack.push(1)
 customstack.push(2)
 customstack.push(3)
 print(customstack)
+print(customstack.peek())
+print(customstack.pop())
 print(customstack.delete())
 
 
